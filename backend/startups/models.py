7d2875ecfc53271f0be2a078586e9616ecc16a8c
MOHAMMED <maa089@cs.bham.ac.uk>
2022-03-06 17:38:44 +0400

    moved the model over to the start-up app

--- backend/startups/models.py
+++ backend/startups/models.py
@@ -1,3 +1,28 @@
 from django.db import models
-
+from django.db import models
+from django.contrib.auth import get_user_model
 # Create your models here.
+
+User = get_user_model()
+
+#This model is for the start-ups that sign up with us
+class Business(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200) #Name type in business table is varChar
+    founders = models.CharField(max_length=200)
+    dateFounded = models.DateField(required=False, null=True)
+    description = models.TextField()
+    email = models.EmailField()
+    num_Employees = models.IntegerField()
+    slug = models.SlugField()
+    logo = models.ImageField(required=False, null=True)
+    INDUSTRY_CHOICES = [
+        ('RE', 'Real Estate'),
+         ('TR', 'Trading'),
+         ('SR', 'Services'),
+         ('TT', 'Travel and Tourism'),
+         ('HS', 'Hospitality'),
+         ('OT', 'Others')
+    ]
+    industry = models.CharField(max_length=2, choices=INDUSTRY_CHOICES, null=True)
+    size = models.IntegerField()
