0315ad163fc74ae86d4eb9da9a27432bfa928c9c
MOHAMMED <maa089@cs.bham.ac.uk>
2022-03-14 19:44:49 +0400

    corrected login gui

--- backend/startups/models.py
+++ backend/startups/models.py
@@ -2,21 +2,22 @@ from django.db import models
 
 #This model is for the start-ups that sign up with us
 class Business(models.Model):
-    name = models.CharField(max_length=200) #Name type in business table is varChar
-    founders = models.CharField(max_length=200)
-    dateFounded = models.DateField(null=True)
-    description = models.TextField()
-    email = models.EmailField()
-    num_Employees = models.IntegerField()
-    slug = models.SlugField()
-    logo = models.ImageField(null=True)
-    INDUSTRY_CHOICES = [
-        ('RE', 'Real Estate'),
-         ('TR', 'Trading'),
-         ('SR', 'Services'),
-         ('TT', 'Travel and Tourism'),
-         ('HS', 'Hospitality'),
-         ('OT', 'Others')
-    ]
-    industry = models.CharField(max_length=2, choices=INDUSTRY_CHOICES, null=True)
-    size = models.IntegerField()
+    # name = models.CharField(max_length=200) #Name type in business table is varChar
+    # founders = models.CharField(max_length=200)
+    # dateFounded = models.DateField(null=True)
+    # description = models.TextField()
+    # email = models.EmailField()
+    # num_Employees = models.IntegerField()
+    # slug = models.SlugField()
+    # logo = models.ImageField(null=True)
+    # INDUSTRY_CHOICES = [
+    #     ('RE', 'Real Estate'),
+    #      ('TR', 'Trading'),
+    #      ('SR', 'Services'),
+    #      ('TT', 'Travel and Tourism'),
+    #      ('HS', 'Hospitality'),
+    #      ('OT', 'Others')
+    # ]
+    # industry = models.CharField(max_length=2, choices=INDUSTRY_CHOICES, null=True)
+    # size = models.IntegerField()
+    pass
