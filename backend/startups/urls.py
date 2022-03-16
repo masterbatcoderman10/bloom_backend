7b1b0820633f9e1d98e73694759e48a6a06d0c71
MOHAMMED <maa089@cs.bham.ac.uk>
2022-03-17 01:18:21 +0400

    commit

--- backend/startups/urls.py
+++ backend/startups/urls.py
@@ -1,21 +1,13 @@
 from django.contrib import admin
 from django.urls import path
 from django.urls import  include
-<<<<<<< HEAD
-<<<<<<< HEAD
+
+
 from . import views
 
 urlpatterns = [
 
     path('', view=views.BusinessListView.as_view()),
     path('<int:pk>/', view=views.BusinessDetailView.as_view())
-=======
-
-urlpatterns = [
->>>>>>> main
-=======
-
-urlpatterns = [
->>>>>>> origin
     
 ]
