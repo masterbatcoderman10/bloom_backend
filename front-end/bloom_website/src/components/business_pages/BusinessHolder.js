2836ffccc578cc7b8ea46cd03fc276d4b7d1165b
MOHAMMED <maa089@cs.bham.ac.uk>
2022-03-29 21:58:36 +0400

    fixed title

--- front-end/bloom_website/src/components/business_pages/BusinessHolder.js
+++ front-end/bloom_website/src/components/business_pages/BusinessHolder.js
@@ -1,8 +1,5 @@
 import React from "react"
-import { useNavigate } from "react-router-dom";
-import { useState, useEffect } from "react";
-import axios from "axios";
-import {Button} from "../Button"
+
 import './BusinessContainer.css'
 import BusinessCard from "./BusinessCard";
 import {Loader} from "../Loader";
