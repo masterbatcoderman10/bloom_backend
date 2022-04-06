2e95f1eb84b6c8b6a784cac8b30575423b3e02af
MOHAMMED <maa089@cs.bham.ac.uk>
2022-04-06 15:03:54 +0400

    added serializer

---
+++ backend/marketplace/serializers.py
@@ -0,0 +1,30 @@
+from dataclasses import fields
+from rest_framework import serializers
+from .models import Vendor
+from .models import Category
+
+class VendorSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Vendor
+        fields = (
+            'id', 
+            'name',
+            'description',
+            'pricing',
+            'category',
+            'logo',
+            'main_link',
+            'account_link',
+            'features'
+        )
+
+
+class CategorySerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Category
+        fields = (
+            'id',
+            'category'
+        )
