70d14a0d58d1546779502c2ef24b20f5265151fa
MOHAMMED <maa089@cs.bham.ac.uk>
2022-04-08 01:13:27 +0400

    modified navbar

--- front-end/bloom_website/src/components/Navbar.js
+++ front-end/bloom_website/src/components/Navbar.js
@@ -9,18 +9,33 @@ function Navbar() {
   const navigate = useNavigate();
   const handleClick = () => setClick(!click);
   const closeMobileMenu = () => setClick(false);
+  const [isNavCollapsed, setIsNavCollapsed] = useState(true);
+
+  const handleNavCollapse = () => setIsNavCollapsed(!isNavCollapsed);
 
   function checkSignedIn() {
-    const token = localStorage.getItem('token');
-    return token ? <button className={`btn`} id="sign-in-btn" onClick={() => 
-      {
-      
-      localStorage.removeItem('token');
-      console.log("broken?")
-      navigate("/")
-    }}>SIGN-OUT</button> :
-    <button className={`btn`} id="sign-in-btn" onClick={() => navigate("/login")}>SIGN-IN</button>
-     
+    const token = localStorage.getItem("token");
+    return token ? (
+      <button
+        className={`btn`}
+        id="sign-in-btn"
+        onClick={() => {
+          localStorage.removeItem("token");
+          console.log("broken?");
+          navigate("/");
+        }}
+      >
+        SIGN-OUT
+      </button>
+    ) : (
+      <button
+        className={`btn`}
+        id="sign-in-btn"
+        onClick={() => navigate("/login")}
+      >
+        SIGN-IN
+      </button>
+    );
   }
 
   const showButton = () => {
@@ -35,55 +50,67 @@ function Navbar() {
     showButton();
   }, []);
 
-  
-
   window.addEventListener("resize", showButton);
 
   return (
     <>
-      <nav className="navbar">
-        <div className="navbar-container">
-          <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
+      <nav 
+        className="navbar navbar-expand-lg navbar-dark"
+        id="bloom-nav">
+        <div className="container-fluid">
+          <Link
+            to="/"
+            className="navbar-brand"
+            id="heading"
+            onClick={closeMobileMenu}
+          >
             BLOOM
           </Link>
-          <div className="menu-icon" onClick={handleClick}></div>
-          <ul className={click ? "nav-menu active" : "nav-menu"}>
-            <li className="nav-item">
-              <Link to="/" className="nav-links" onClick={closeMobileMenu}>
-                Home
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link
-                to="/marketplace"
-                className="nav-links"
-                onClick={closeMobileMenu}
-              >
-                Marketplace
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link
-                to="/startups"
-                className="nav-links"
-                onClick={closeMobileMenu}
-              >
-                Dashboard
-              </Link>
-            </li>
+          {/* <div className="menu-icon" onClick={handleClick}></div> */}
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded={!isNavCollapsed ? true : false}
+            aria-label="Toggle navigation"
+            onClick={handleNavCollapse}
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div
+            className={`${isNavCollapsed ? "collapse" : ""} navbar-collapse`}
+            id="navbarNav"
+          >
+            <ul className="navbar-nav ms-auto">
+              <li className="nav-item nav-li">
+                <Link to="/" className="nav-link" onClick={closeMobileMenu}>
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item nav-li">
+                <Link
+                  to="/marketplace"
+                  className="nav-link"
+                  onClick={closeMobileMenu}
+                >
+                  Marketplace
+                </Link>
+              </li>
+              <li className="nav-item nav-li">
+                <Link
+                  to="/startups"
+                  className="nav-link"
+                  onClick={closeMobileMenu}
+                >
+                  Dashboard
+                </Link>
+              </li>
 
-            <li>
-              <Link
-                to="/products"
-                className="nav-links-mobile"
-                onClick={closeMobileMenu}
-              >
-                Sign Up
-              </Link>
-            </li>
-          </ul>
-          {checkSignedIn()}
-          
+              <li className="nav-item nav-li">{checkSignedIn()}</li>
+            </ul>
+          </div>
         </div>
       </nav>
     </>
