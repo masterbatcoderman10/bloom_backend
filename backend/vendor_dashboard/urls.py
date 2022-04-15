b24458830e078d408ffcb76f2cd6e6db08a8ace0
MOHAMMED <maa089@cs.bham.ac.uk>
2022-04-15 15:32:17 +0400

    added post dashboard membership

--- backend/vendor_dashboard/urls.py
+++ backend/vendor_dashboard/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('<int:startupID>/isPresent/', view=views.DashboardConfirmView.as_view()),
-    path('<int:dashID>/vendorList/', view=views.MembersListView.as_view())
+    path('<int:dashID>/vendorList/', view=views.MembersListView.as_view()),
+    path('<int:startupID>/<int:vendorID>/addMember/', view=views.MemberDetailView.as_view())
 ]
---
+++ backend/vendor_dashboard/views/post_membership.py
@@ -0,0 +1,48 @@
+from html5lib import serialize
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import APIView, api_view
+from rest_framework import permissions
+from marketplace.models import Vendor
+from marketplace.serializers import VendorSerializer
+from ..serializers import DashboardSerializer
+from ..serializers import MembershipSerlializer
+from ..models import Dashboard
+from ..models import Membership
+from startups.models import Business
+
+class MemberDetailView(APIView):
+
+    permission_classes = [permissions.IsAuthenticated]
+
+    def post(self, request, startupID, vendorID):
+
+        data = {}
+
+        try: 
+            business = Business.objects.get(pk=startupID)
+        except Business.DoesNotExist:
+
+            data["message"] = "Business does not exist"
+            return Response(data, status.HTTP_400_BAD_REQUEST)
+        
+        if request.user != business.user:
+            data["message"] = "Not authorized"
+            return Response(data, status.HTTP_401_UNAUTHORIZED)
+
+        try:
+            Vendor.objects.get(pk=vendorID)
+        except:
+            Vendor.DoesNotExist
+            data["message"] = "This vendor does not exist"
+            return Response(data, status.HTTP_404_NOT_FOUND)
+
+        Dashboard.objects.get_or_create(startup_id=startupID)
+        dashboard = Dashboard.objects.get(startup_id=startupID)
+
+        Membership.objects.get_or_create(dashboard_id=dashboard.id, vendor_id=vendorID)
+        data["messages"] = "membership created"
+
+        return Response(data, status.HTTP_200_OK)
+
+        
