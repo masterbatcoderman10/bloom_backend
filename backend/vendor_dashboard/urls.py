8601ad6fa2fe416fcdbe3db24ecd48ddc1364652
MOHAMMED <maa089@cs.bham.ac.uk>
2022-05-03 15:12:00 +0400

    modified urls

--- backend/vendor_dashboard/urls.py
+++ backend/vendor_dashboard/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<int:startupID>/isPresent/', view=views.DashboardConfirmView.as_view()),
-    path('<int:dashID>/vendorList/', view=views.MembersListView.as_view()),
-    path('<int:startupID>/<int:vendorID>/addMember/', view=views.MemberDetailView.as_view())
+    path('<int:startupID>/exists/', view=views.DashboardConfirmView.as_view()),
+    path('<int:dashID>/members/', view=views.MembersListView.as_view()),
+    path('<int:startupID>/<int:vendorID>/details/', view=views.MemberDetailView.as_view())
 ]
--- backend/vendor_dashboard/views/post_membership.py
+++ backend/vendor_dashboard/views/post_membership.py
@@ -1,4 +1,5 @@
 from html5lib import serialize
+from numpy import delete
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import APIView, api_view
@@ -42,6 +43,36 @@ class MemberDetailView(APIView):
 
         Membership.objects.get_or_create(dashboard_id=dashboard.id, vendor_id=vendorID)
         data["messages"] = "membership created"
+
+        return Response(data, status.HTTP_200_OK)
+    
+    def delete(self, request, startupID, vendorID):
+
+        data = {}
+
+        try: 
+            business = Business.objects.get(pk=startupID)
+        except Business.DoesNotExist:
+
+            data["message"] = "Business does not exist"
+            return Response(data, status.HTTP_400_BAD_REQUEST)
+        
+        if request.user != business.user:
+            data["message"] = "Not authorized"
+            return Response(data, status.HTTP_401_UNAUTHORIZED)
+
+        try:
+            Vendor.objects.get(pk=vendorID)
+        except:
+            Vendor.DoesNotExist
+            data["message"] = "This vendor does not exist"
+            return Response(data, status.HTTP_400_BAD_REQUEST)
+        
+        Dashboard.objects.get_or_create(startup_id=startupID)
+        dashboard = Dashboard.objects.get(startup_id=startupID)
+
+        Membership.objects.delete(dashboard_id=dashboard.id, vendor_id=vendorID)
+        data["messages"] = "membership deleted"
 
         return Response(data, status.HTTP_200_OK)
 
